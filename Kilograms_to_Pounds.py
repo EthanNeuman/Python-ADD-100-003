5888d2eefccf8d5bb395379a088c6678ddc5c469
Ethan Neuman <eneuman4870@students.mchjenry.edu>
2025-03-01 14:53:33 -0600

    Kilograms to pounds

---
+++ Kilograms_to_Pounds.py
@@ -0,0 +1,16 @@
+kg_value_1 = 4
+kg_value_2 = 9
+kg_value_3 = 15.5
+kg_value_4 = 22
+
+conversion_factor = 2.20462
+
+lb_1 = kg_value_1 * conversion_factor
+lb_2 = kg_value_2 * conversion_factor
+lb_3 = kg_value_3 * conversion_factor
+lb_4 = kg_value_4 * conversion_factor
+
+print(f"{kg_value_1} kg is equal to {lb_1:.4f} lb.")
+print(f"{kg_value_2} kg is equal to {lb_2:.4f} lb.")
+print(f"{kg_value_3} kg is equal to {lb_3:.4f} lb.")
+print(f"{kg_value_4} kg is equal to {lb_4:.4f} lb.")
