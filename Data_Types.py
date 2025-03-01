3a283c52d03f8b7e9e736bff4de4d84fcd0dca24
Ethan Neuman <eneuman4870@students.mchjenry.edu>
2025-03-01 10:22:49 -0600

    Data Types in Python

---
+++ Data_Types.py
@@ -0,0 +1,20 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+print("Literal:", 110)
+
+# Integer
+y = 100
+print("Integer:", y)
+
+# Float
+z = 97.97
+print("Float:", z)
+
+# String
+name = "Korey"
+print("String:", name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
