ba1b44299e13689b7bb91e26dbe955b4b243d11b
EthanNeuman <113403874+EthanNeuman@users.noreply.github.com>
2025-03-01 09:34:50 -0600

    Add files via upload

---
+++ knock-knock-joke.py
@@ -0,0 +1,10 @@
+def knock_knock_joke():
+    print("Knock, knock!")
+    input()
+    print("Tank.")
+    input()
+    print("You're welcome!")
+
+
+if __name__ == "__main__":
+    knock_knock_joke()
