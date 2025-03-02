6bd565578249d37765974b426abb83a7e1906a7f
Ethan Neuman <eneuman4870@students.mchjenry.edu>
2025-03-02 13:21:55 -0600

    comments

---
+++ comments.py
@@ -0,0 +1,8 @@
+# Define the size of the fuel tank in gallons
+tank_size = 15
+# Define the total distance covered in miles
+distance = 600
+# Calculate miles per gallon (mpg) by dividing distance by tank size
+mpg = distance / tank_size
+# Print the calculated miles per gallon value
+print("Miles per gallon:", mpg)
